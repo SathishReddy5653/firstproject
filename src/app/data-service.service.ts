5fbd10f1612b815cf1f10a66ceb2c12dffb7fd3d
sathish <sathishreddy2902@gmail.com>
2022-10-26 11:21:49 +0530

    completed databinding

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -4,7 +4,28 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataServiceService {
-  
-
   constructor() { }
+  getemployees() {
+    return [
+      {
+        firstName: 'sathish',
+        lastName: 'reddy m',
+        email:'sathish@gmail.com'
+      },
+      {
+        firstName: 'sai kiran',
+        lastName: 'martin reddy',
+        email:'saikiranreddy@gmail.com'
+      },
+      {
+        firstName: 'jeevitha ',
+        lastName: 'reddy marri',
+        email:'jeevithareddy@gmail.com'
+      },
+      {
+        firstName: 'Madhu sudhan ',
+        lastName: 'Reddy marri',
+        email:'madhureddy@gmail.com'
+      },
+    ]}
 }
